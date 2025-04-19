efef7723ea403a279c385b87623c90cdfeeb75df
Zhansar <daid8002@gmail.com>
2025-04-19 20:33:40 +0500

    fix: gitignore

--- backend/target/debug/backend.d
+++
@@ -1,1 +0,0 @@
-/Users/zhansarsadriev/Developer/rust/web_server/backend/target/debug/backend: /Users/zhansarsadriev/Developer/rust/web_server/backend/src/main.rs
--- backend/target/debug/deps/backend-ab2bb47fdbb29d38.d
+++
@@ -1,5 +0,0 @@
-/Users/zhansarsadriev/Developer/rust/web_server/backend/target/debug/deps/backend-ab2bb47fdbb29d38: src/main.rs
-
-/Users/zhansarsadriev/Developer/rust/web_server/backend/target/debug/deps/backend-ab2bb47fdbb29d38.d: src/main.rs
-
-src/main.rs:
